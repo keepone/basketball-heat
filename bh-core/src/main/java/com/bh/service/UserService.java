1b721902decaf647e034cec20f2a77c499dd983a
唐定 <Allen@tangdingdeMacBook-Pro.local>
2016-11-20 16:33:44 +0800

    don't update anything

---
+++ bh-core/src/main/java/com/bh/service/UserService.java
@@ -0,0 +1,9 @@
+package com.bh.service;
+
+import com.bh.model.User;
+
+ 
+
+public interface UserService {
+	public int insertUser(User user);
+}
---
+++ bh-dao/src/main/java/com/bh/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.bh.dao;
+
+import com.bh.model.User;
+
+
+public interface UserDao {
+	public int insertUser(User user);
+}
